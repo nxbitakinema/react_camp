416afc72a8df46bf30f92fc885f57b9d27a47284
nxbitakinema <nxbitakinema@gmail.com>
2026-04-26 14:34:07 +0700

    build and clean project / install tailwind, shadcn

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="ghost" size="sm">Click me</Button>
+    </div>
+  )
+}
+export default App
